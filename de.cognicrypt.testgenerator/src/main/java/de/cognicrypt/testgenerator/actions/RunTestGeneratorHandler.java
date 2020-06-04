cbeaa1cddf58e0f1f66910b232bd8ee4fcf1b216
Rakshit Krishnappa Ravi <rakshitkrishnapparavi@gmail.com>
2020-06-04 22:55:22 +0200

    Removed unrequired copyright
    Signed-off-by: Rakshit Krishnappa Ravi <rakshitkrishnapparavi@gmail.com>

--- de.cognicrypt.testgenerator/src/main/java/de/cognicrypt/testgenerator/actions/RunTestGeneratorHandler.java
+++ de.cognicrypt.testgenerator/src/main/java/de/cognicrypt/testgenerator/actions/RunTestGeneratorHandler.java
@@ -1,13 +1,3 @@
-/********************************************************************************
- * Copyright (c) 2015-2019 TU Darmstadt, Paderborn University
- * 
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
- * 
- * SPDX-License-Identifier: EPL-2.0
- ********************************************************************************/
-
 package de.cognicrypt.testgenerator.actions;
 
 import org.eclipse.core.commands.AbstractHandler;
