e28263bbf359dda4f458c84a3689f2a3fff1e763
Rakshit Krishnappa Ravi <rakshitkrishnapparavi@gmail.com>
2020-06-11 15:01:59 +0200

    Readded full class names
    Signed-off-by: Rakshit Krishnappa Ravi <rakshitkrishnapparavi@gmail.com>
    
    - isSubType method requires full class names for comparison
    - overrided addStatementToBody() to add simple class names to the body statements

--- de.cognicrypt.testgenerator/src/main/java/de/cognicrypt/testgenerator/generator/GeneratorTestMethod.java
+++ de.cognicrypt.testgenerator/src/main/java/de/cognicrypt/testgenerator/generator/GeneratorTestMethod.java
@@ -1,6 +1,7 @@
 package de.cognicrypt.testgenerator.generator;
 
 import java.util.List;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.Map.Entry;
 
 import de.cognicrypt.codegenerator.generator.GeneratorMethod;
@@ -25,5 +26,22 @@ public class GeneratorTestMethod extends GeneratorMethod {
 				}
 			}
 		}
+	}
+	
+	public void addStatementToBody(String statement) {
+		int index = -1;
+		if ((index = statement.indexOf('=')) > 0) {
+			String[] varDecl = statement.substring(0, index).split(" ");
+			if (varDecl.length == 2) {
+				SimpleEntry<String, String> newVar = new SimpleEntry<>(varDecl[1], varDecl[0]);
+				if (!getDeclaredVariables().contains(newVar)) {
+					getDeclaredVariables().add(newVar);
+				}
+				String simpleVarType = Utils.retrieveOnlyClassName(newVar.getValue());
+				statement = simpleVarType + " " + varDecl[1] + " = " + statement.split(" = ")[1];
+			}
+		}
+		body.append(statement);
+		body.append("\n");
 	}
 }
