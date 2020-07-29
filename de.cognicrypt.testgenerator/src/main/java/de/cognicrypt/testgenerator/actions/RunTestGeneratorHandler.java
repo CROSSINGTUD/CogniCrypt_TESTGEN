e1efcf3a772e99f3a1290f649c66faa9dbead75f
Rakshit Krishnappa Ravi <rakshitkrishnapparavi@gmail.com>
2020-07-29 20:52:44 +0200

    Added method to close editors
    Signed-off-by: Rakshit Krishnappa Ravi <rakshitkrishnapparavi@gmail.com>
    
    - fixes errors occuring during clean up

--- de.cognicrypt.testgenerator/src/main/java/de/cognicrypt/testgenerator/actions/RunTestGeneratorHandler.java
+++ de.cognicrypt.testgenerator/src/main/java/de/cognicrypt/testgenerator/actions/RunTestGeneratorHandler.java
@@ -3,6 +3,11 @@ package de.cognicrypt.testgenerator.actions;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 
 import de.cognicrypt.testgenerator.generator.TestGenerator;
 
@@ -10,9 +15,20 @@ public class RunTestGeneratorHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		closeExistingEditors();
 		TestGenerator generator = TestGenerator.getInstance();
 		generator.generateTests();	
 		return event;
+	}
+
+	public void closeExistingEditors() {
+		IWorkbenchWindow workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		IWorkbenchPage page = workbenchWindow.getActivePage();
+		IEditorReference[] editorRefs = page.getEditorReferences();
+		for (int i = 0; i < editorRefs.length; i++) {
+			IEditorPart editor = editorRefs[i].getEditor(true);
+			page.closeEditor(editor, true);
+		}
 	}
 
 }
